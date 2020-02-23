69204755a307b8201a0df89592fb612ba9246575
Paul Harnack <harna100@mail.chapman.edu>
2020-02-23 14:07:03 -0800

    Modifies post create to use a form instead of two way binding

--- mean-course/src/app/posts/post-create/post-create.component.ts
+++ mean-course/src/app/posts/post-create/post-create.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { Post } from 'src/app/models/post.model';
+import { NgForm } from '@angular/forms';
 
 @Component({
     selector: 'app-post-create',
@@ -11,10 +12,13 @@ export class PostCreateComponent {
     enteredContent  = '';
     @Output() postCreated = new EventEmitter<Post>();
 
-    onAddPost() {
+    onAddPost(form: NgForm) {
+        if (form.invalid) {
+            return;
+        }
         const newPost: Post = {
-            title: this.enteredTitle,
-            content: this.enteredContent
+            title: form.value.title,
+            content: form.value.content
         };
         this.postCreated.emit(newPost);
     }

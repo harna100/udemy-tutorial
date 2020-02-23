0e2ff8fc209501d7ca04ee561bccd5e293a6a21e
Paul Harnack <harna100@mail.chapman.edu>
2020-02-23 15:54:37 -0800

    Adds initial post api endpoint

--- mean-course/backend/app.js
+++ mean-course/backend/app.js
@@ -2,13 +2,21 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log("First Middleware")
-    next();
-});
-
-app.use((req, res, next) => {
-    res.send('Hello from express!');
+app.use('/api/posts', (req, res, next) => {
+    const posts = [
+        {id: 'dfsiugnbsdfn',
+         title: 'Server Post',
+         content: 'Server content'
+        },
+        {id: 'dfgsoindfglk',
+        title: 'Server Post',
+        content: 'Server content'
+       },
+    ];
+    res.status(200).json({
+        message: 'Posts fetched successfully',
+        posts: posts
+    });
 });
 
 module.exports = app;
